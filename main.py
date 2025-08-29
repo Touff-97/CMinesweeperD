4d974266340269a703ba82301bf0511dbde21dd6
schristen <s.christen.marote@gmail.com>
2025-08-29 16:47:14 +0200

    can now change state in tiles, started working on counting adjacent bombs

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import random
 
 class Tile:
-    def __init__(self, tile_type):
+    def __init__(self, column, row, is_bomb):
+        self.column = column
+        self.row = row
         self.display = "[ ]"
-        self.type = tile_type
-        self.discovered = True
+        self.is_discovered = True
+        self.is_bomb = is_bomb
+        self.is_flagged = False
         self.adjacent_bombs = 0
+        self.type = self.update_state()
         self.update_display()
 
     def __repr__(self):
-        return self.display.format(self.adjacent_bombs) if self.discovered else "[ ]"
+        return self.display.format(self.adjacent_bombs) if self.is_discovered else "[ ]"
 
     def update_display(self):
         match self.type:
@@ -23,6 +27,18 @@ class Tile:
                 self.display = "[F]"
             case 4:
                 self.display = "[{}]"
+
+    def update_state(self):
+        if not self.is_discovered:
+            return 0
+        if self.is_flagged:
+            return 3
+        if self.is_bomb:
+            return 2
+        if self.adjacent_bombs <= 0:
+            return 1
+        return 4
+
 
 
 class Board:
@@ -48,8 +64,16 @@ class Board:
         for x in range(self.width):
             column = []
             for y in range(self.height):
-                column.append(Tile(random.randint(0, 4))) # Add a tile to the board
+                column.append(Tile(x, y, random.randint(0, 4))) # Add a tile to the board
             self.tiles.append(column)
+
+    def count_bombs(self):
+        for tile in self.tiles:
+            bombs = 0
+            for i in range(-1, 1, 1):
+                for j in range(-1, 1, 1):
+                    bombs += 1 if self.tiles[tile.column + i][tile.row + i].is_bomb else 0
+            tile.adjacent_bombs = bombs
 
 board = Board(10, 10)
 board.populate_board()
